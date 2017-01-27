176a3f529ac79f50939b426a39c480a0c3619e94
Lisa Ruby <lruby1203@hotmail.com>
2017-01-26 20:32:35 -0600

    Solution to Studio3

--- main.py
+++ main.py
@@ -101,17 +101,20 @@ class AddMovie(webapp2.RequestHandler):
         # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
 
-        # TODO 2
-        # if the user typed nothing at all, redirect and yell at them
-
+        blank = "Cannot add movie without title"
+        No = "{0} is a terrible movie. It will not be added to your watchlist.".format(new_movie)
+        if new_movie == "":
+            self.redirect("/?error=" + blank)
+        if new_movie in terrible_movies:
+            self.redirect("/?error=" + No)
 
         # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
 
 
-        # TODO 1
-        # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
 
+
+        new_movie = cgi.escape(new_movie)
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
         sentence = new_movie_element + " has been added to your Watchlist!"
